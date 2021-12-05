72a65f607487ffeddd8658c4cf41b469ead1dc1f
Devabubakar <abubakarali3w@gmail.com>
2021-12-05 20:41:43 +0300

    updated app to be pwa compliant

--- src/index.js
+++ src/index.js
@@ -3,9 +3,13 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+serviceWorkerRegistration.register();
