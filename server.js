61ce20876efaae9e8b434a7e1221801b2a494b25
Devabubakar <abubakarali3w@gmail.com>
2021-12-07 13:38:12 +0300

    bug:fix 404 heroku :

--- server.js
+++ server.js
@@ -9,9 +9,13 @@ app.listen(PORT, (req, res) => {
   console.log(`Server running on port : ${PORT}`);
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 if (process.env.NODE_ENV === 'production') {
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
   app.use(compression());
+
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function (req, res) {
