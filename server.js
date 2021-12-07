243f15f75f0f6c52893e38a40888b80cf612a8f6
Devabubakar <abubakarali3w@gmail.com>
2021-12-07 14:04:44 +0300

    added path module to server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const enforce = require('express-sslify');
 const compression = require('compression');
-
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
